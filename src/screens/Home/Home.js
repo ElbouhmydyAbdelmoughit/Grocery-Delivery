fa2ec90ab53d86141c28d285ec05f33dd282ab56
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-01-26 16:18:26 +0000

    create BoxItemCategories components for show all my categories in home screen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {StyleSheet, Text, View, useColorScheme, StatusBar} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+  StatusBar,
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import Header from '../../components/molecules/Header/Header';
 import {
   fonts,
@@ -14,7 +22,8 @@ import {
   IL_Grapes_PNG,
   IL_Greentea_PNG,
   IL_Tomato_PNG,
-} from '../../res';
+} from '../../res/images';
+import BoxItemCategories from '../../components/molecules/BoxItemCategories/BoxItamCaterories';
 
 const Home = ({navigation}) => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -81,20 +90,90 @@ const Home = ({navigation}) => {
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.flex1}>
+        {/* Header */}
         <Header drawer />
+        <ScrollView>
+          {/* Search */}
+          <View style={styles.SearchHeader}>
+            <View style={styles.SearchView}>
+              <TextInput placeholder="Search" style={styles.InputSearch} />
+              <IC_Search />
+            </View>
+          </View>
+        </ScrollView>
       </View>
+      {/* categories */}
+      <View>
+        <Text style={styles.titleCategories}>Categories</Text>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.scrollViewCategories}>
+          <BoxItemCategories
+            icon={<IC_Fruits />}
+            color="rgba(169, 178, 169, 0.5)"
+            text="Fruits"
+            onPress={() => navigation.navigate('Categories', 'Fruits')}
+          />
+          <BoxItemCategories
+            icon={<IC_Vegetables />}
+            color="rgba(233, 255, 210, 0.5)"
+            text="Vegetables"
+            onPress={() => navigation.navigate('Categories', 'Vegetables')}
+          />
+          <BoxItemCategories
+            icon={<IC_Drinks />}
+            color="rgba(140, 175, 53, 0.5)"
+            text="Drinks"
+            onPress={() => navigation.navigate('Categories', 'Drinks')}
+          />
+          <BoxItemCategories
+            icon={<IC_Bakery />}
+            color="rgba(214, 255, 218, 0.5)"
+            text="Bakery"
+            onPress={() => navigation.navigate('Categories', 'Bakery')}
+          />
+          <BoxItemCategories
+            icon={<IC_Bakery2 />}
+            color="rgba(255, 250, 204, 0.5)"
+            text="Bakery"
+            onPress={() => navigation.navigate('Categories', 'Bakery')}
+          />
+        </ScrollView>
+      </View>
+      <Gap height={24} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1,
   },
-  flex1:{
-    flex:1
-  }
-  
+  flex1: {
+    flex: 1,
+  },
+  SearchHeader: {
+    paddingHorizontal: 20,
+  },
+  SearchView: {
+    height: 40,
+    backgroundColor: '#DDDDDD',
+    borderRadius: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+  },
+  titleCategories: {
+    fontSize: 18,
+    fontFamily: fonts.SemiBold,
+    color: colors.primary,
+    padding: 20,
+  },
+  scrollViewCategories: {
+    paddingLeft: 20,
+  },
 });
 
 export default Home;
